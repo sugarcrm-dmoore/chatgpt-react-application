54086ad0a92399363dc9bd2c5f86ce69d2e3adcd
Daniel Moore <daniel.moore@sugarcrm.com>
2023-03-27 09:26:44 -0400

    adds input bar

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -12,18 +12,7 @@ import { InsightCard } from '../InsightCard/InsightCard';
 type ModelValueType = 'gpt' | 'codex' | 'image';
 const App = () => {
   const [isShowing, setIsShowing] = useState(false);
-  const [responseList, setResponseList] = useState<ResponseInterface[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
   let loadInterval: number | undefined;
-
-
-  const generateUniqueId = () => {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
-
-    return `id-${timestamp}-${hexadecimalString}`;
-  }
 
   const htmlToText = (html: string) => {
     const temp = document.createElement('div');
@@ -31,73 +20,9 @@ const App = () => {
     return temp.textContent;
   }
 
-  const delay = (ms: number) => {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-  }
-
-  const addResponse = (selfFlag: boolean, response?: string) => {
-    const uid = generateUniqueId()
-    setResponseList(prevResponses => [
-      ...prevResponses,
-      {
-        id: uid,
-        response,
-        selfFlag
-      },
-    ]);
-    return uid;
-  }
-
-  const updateResponse = (uid: string, updatedObject: Record<string, unknown>) => {
-    setResponseList(prevResponses => {
-      const updatedList = [...prevResponses]
-      const index = prevResponses.findIndex((response) => response.id === uid);
-      if (index > -1) {
-        updatedList[index] = {
-          ...updatedList[index],
-          ...updatedObject
-        }
-      }
-      return updatedList;
-    });
-  }
-
   const clickHandler = useCallback(() => {
     setIsShowing(!isShowing);
-    getGPTResult();
   }, [isShowing])
-
-  const getGPTResult = async () => {
-    // Get the prompt input
-    const _prompt = 'Rystad Energy';
-
-    setIsLoading(true);
-
-    let uniqueId: string;
-    uniqueId = addResponse(false);
-    await delay(50);
-
-    try {
-      // Send a POST request to the API with the prompt in the request body
-      const response = await axios.post('get-prompt-result', {
-        prompt: _prompt,
-      });
-      setIsLoading(false)
-      updateResponse(uniqueId, {
-        response: response.data.trim(),
-      });
-    } catch (err) {
-      updateResponse(uniqueId, {
-        // @ts-ignore
-        response: `Error: ${err.message}`,
-        error: true
-      });
-    } finally {
-      // Clear the loader interval
-      clearInterval(loadInterval);
-      setIsLoading(false);
-    }
-  }
 
   return (
     <div className='App'>
@@ -133,26 +58,7 @@ const App = () => {
             </div>
             <div className={'card-area'}>
               { focusConfig.map(c => (<InsightCard {...c}/>)) }
-              {/* <div className={'insight-card scroll-y'}>
-                <div className='sugarcrm-loader'></div> */}
-                {/* {isLoading && (<div className='sugarcrm-loader'></div>)}
-                <div id="response-list">
-                  <PromptResponseList responseList={responseList} key="response-list"/>
-                </div> */}
-              {/* </div> */}
-              {/* <div className={'insight-card scroll-y'}>
-                {isLoading && (<div className='sugarcrm-loader'></div>)}
-                <div id="response-list">
-                  <PromptResponseList responseList={responseList} key="response-list"/>
-                </div>
-              </div> */}
             </div>
-            {/* <div className={'full-area scroll-y'}>
-            {isLoading && (<div className='sugarcrm-loader'></div>)}
-              <div id="response-list">
-                <PromptResponseList responseList={responseList} key="response-list"/>
-              </div>
-            </div> */}
           </div>)}
         </div>
       </div>
